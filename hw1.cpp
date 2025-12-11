be41650e6f0b85948952e33f2b7af9b051e99b7f
beembeem <nutsummer003@gmail.com>
2025-12-11 20:27:51 +0700

    Add files via upload

---
+++ hw1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main ()
+{
+	int hour,min,sec;
+	scanf("%d:%d:%d:",&hour,&min,&sec);
+	printf("hour : %d\n",hour);
+	printf("min : %d\n",min);
+	printf("sec : %d\n",sec);
+	
+}
